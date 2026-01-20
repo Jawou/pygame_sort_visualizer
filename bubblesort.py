f7f65482d93336e8a41d4ee803eef99b6669786f
Jawou <88535234+Jawou@users.noreply.github.com>
2026-01-21 00:28:26 +0100

    Refactor screen update and improve sorting speed
    Updated the screen drawing method to use rectangles for better visualization and increased the clock tick rate during sorting.

--- bubblesort.py
+++ bubblesort.py
@@ -6,7 +6,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 clock = pygame.time.Clock() 
-#"numbers" is a list, put in None matching list if you dont want to use that "feature"
+
 class numbers_displayer:
   def __init__(self, WIDTH, HEIGHT, numbers, scale, line_width, window_name, matching_list):
     self.WIDTH = WIDTH
@@ -18,26 +18,28 @@ class numbers_displayer:
     self.numbers_length = len(numbers)
     self.matching_list = matching_list
     pygame.display.set_caption(window_name)
-
+ 
   def update_screen(self):
-     place_increment = self.WIDTH / self.numbers_length
-     place = place_increment / 2
-     self.screen.fill(WHITE)    
-     for i in range(self.numbers_length):
-         if not self.matching_list:
-             color = BLACK
-         else:
-             color = GREEN if self.matching_list[i] == self.numbers[i] else RED
-         pygame.draw.line(
-             self.screen, 
-             color, 
-             (place, self.HEIGHT - self.numbers[i] * self.scale), 
-             (place, self.HEIGHT), 
-             self.line_width
-         )
-         place += place_increment
-     pygame.display.flip()
-
+         self.screen.fill(BLACK)    
+         for i in range(self.numbers_length):
+             current_x = int(i * self.WIDTH / self.numbers_length)
+             next_x = int((i + 1) * self.WIDTH / self.numbers_length)
+             width = next_x - current_x 
+             if not self.matching_list:
+                 color = WHITE
+             else:
+                 color = GREEN if self.matching_list[i] == self.numbers[i] else RED
+             pygame.draw.rect(
+                 self.screen, 
+                 color, 
+                 (
+                     current_x, 
+                     self.HEIGHT - (self.numbers[i] * self.scale), 
+                     width, 
+                     self.numbers[i] * self.scale
+                 )
+             )
+         pygame.display.flip()
 
 random_numbers = []
 for i in range(50):
@@ -48,7 +50,7 @@ screen = numbers_displayer(360, 480, random_numbers, 1, 5, "horrible-sort", sort
 
 for i in range(screen.numbers_length):
     for u in range(screen.numbers_length-i-1):
-        clock.tick(30)
+        clock.tick(240)
         if random_numbers[u] > random_numbers[u+1]:
             temp = random_numbers[u]
             random_numbers[u] = random_numbers[u+1]
